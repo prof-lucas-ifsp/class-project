0bf6b7506f50fd0995aed5c8a53df153e537636b
prof-lucas-ifsp <lucas.oliveira@ifsp.edu.br>
2022-11-30 12:04:32 -0300

    modify tc1 class

--- Tc1.java
+++ Tc1.java
@@ -1,9 +1,5 @@
 public class Tc1 {
    public static void main(String[] args){
-<<<<<<< HEAD
-       System.out.println("Hello TC1 - Turma do Prof. Lucas!!!");
-=======
-       System.out.println("Hello TC1S5!!!");
->>>>>>> 8b7b4b2f2047d913df5081810422f907129a3348
+       System.out.println("Hello TC1S5 - Turma do Prof. Lucas!!!");
    }
 }
